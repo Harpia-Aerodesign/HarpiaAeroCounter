b1b306d39f105dbf2ee93a4fa5ccfb45a5981f10
André Meneghelli Vale <andre.vale@healthbit.com.br>
2021-06-06 11:30:08 -0300

    Inserindo formatação na saída.

--- harpia_aero_counter.py
+++ harpia_aero_counter.py
@@ -1,6 +1,9 @@
+from posixpath import join
 import urllib
 import os
+import re
 
+from pprint import pprint
 from glob import glob
 from subprocess import Popen, DEVNULL, STDOUT
 from git_manager import GitManager
@@ -8,9 +11,32 @@ from config import OVERLEAF_PASSWORD, OVERLEAF_USER, PROJECTS_OVERLEAF, SAE_COUN
 
 
 class HarpiaAeroCounter:
+    LOG_HEAD = [
+        'Words',
+        'Non-Words',
+        'Figures Files',
+        'Fig. Words',
+        'Non-Words Fig.',
+        'Num. of Pages',
+        'Num. of Special Pages',
+        'Words in Math Mode'
+    ]
+
+    LOG_KEYS = [
+        'words',
+        'non_words',
+        'fig_files',
+        'fig_words',
+        'non_words_fig',
+        'npages',
+        'n_special_pages',
+        'math_words'
+    ]
+
     def __init__(self):
         self.sae_counter_manager = GitManager(SAE_COUNTER_GITHUB, SAE_COUNTER_PATH)
         self.projects_list = PROJECTS_OVERLEAF
+        self.content = {}
         for project in self.projects_list:
             project['repo'] = GitManager(
                 project['url'],
@@ -27,7 +53,7 @@ class HarpiaAeroCounter:
         for project in self.projects_list:
             project['repo'].pull()
             if project['repo'].has_changes or redo:
-                print(project['name'], "\t\t    mudou")
+                print("{name}{spaces}mudou".format(name=project['name'], spaces=" "*(40-len(project['name']))))
                 # Compilando o PDF.
                 Popen(
                     ["pdflatex", "-interaction=nonstopmode", "main.tex"],
@@ -51,12 +77,27 @@ class HarpiaAeroCounter:
                         fname,
                         os.path.join(UNTRACKED_PATH, project['path']+"_"+fname.split("/")[-1])
                     )
-                # Mostrar os resultados
             else:
-                print(project['name'], "\t\tnão mudou")
+                print("{name}{spaces}ok".format(name=project['name'], spaces=" "*(43-len(project['name']))))
+            with open(os.path.join(UNTRACKED_PATH, project['path']+"_logfile.txt"), 'r') as fd:
+                lines = fd.readlines()
+                int_values = [int(s) for s in " ".join(lines).split() if s.isdigit()]
+                self.content[project['path']] = dict(zip(HarpiaAeroCounter.LOG_KEYS, int_values))
         redo = False
+
+    def print(self):
+        print("\n")
+        print("#"*230)
+        print("#" + "\t\t".join(["\t\t\t"] + HarpiaAeroCounter.LOG_HEAD) + "   #")
+        print("#" + "-"*228 + "#")
+        for key, values in self.content.items():
+            posfix = "\t" * int((29-len(key))/8)
+            print("# {key}{posfix}\t\t{words}\t\t{non_words}\t\t\t{fig_files}\t\t\t{fig_words}\t\t\t{non_words_fig}\t\t\t{npages}\t\t\t{n_special_pages}\t\t\t\t{math_words}\t             #".format(key=key, posfix=posfix, **values))
+        print("#"*230)
+        print("\n")
 
 
 if __name__ == "__main__":
     harpiaAeroCounter = HarpiaAeroCounter()
     harpiaAeroCounter.loop()
+    harpiaAeroCounter.print()
